8b968fbc72af392686b209a96ca3a357536782d8
Thomas Dowling <thomas@thomas-Vostro-2520.(none)>
2014-03-31 09:42:19 +0100

    Third commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Designed by Jakki Donovan and Thomas Dowling
---
+++ js/main.js
@@ -0,0 +1,67 @@
+var sndOn = false;
+$(window).load(function() {
+
+    $(".top-demo div").each(function() {
+        $(this).circulate({
+            speed: Math.floor(Math.random()*300) + 100,
+            height: Math.floor(Math.random()*1000) - 470,
+            width: Math.floor(Math.random()*1000) - 470
+        })
+    }).mouseover(function() {
+        $(this).circulate({
+            speed: Math.floor(Math.random()*300) + 100,
+            height: Math.floor(Math.random()*1000) - 470,
+            width: Math.floor(Math.random()*1000) - 470
+        })
+    });
+
+    $('#myButton').click(function() {
+
+        $(".top-demo div").each(function() {
+            $(this).circulate({
+                speed: Math.floor(Math.random()*300) + 100,
+                height: Math.floor(Math.random()*1000) - 470,
+                width: Math.floor(Math.random()*1000) - 470
+            });
+
+        });
+
+        jakkimodule.playSound('./sounds/harpRoll.wav', 0.5,sndOn);
+    });
+});
+
+var jakkimodule= (function () {
+
+    return {
+        playSound:  function(str, vol,sndOn) {
+
+            if(sndOn) {
+            var snd = new Audio();
+            snd.src = str;
+            snd.volume = vol;
+            snd.play();
+
+            }
+        },
+
+        switchSound:function  () {
+
+        if(sndOn) {
+            sndOn = false;
+            document.getElementById('mySoundButton').innerHTML = "Turn Sound On"
+        }
+
+        else  if(sndOn ==false) {
+            sndOn = true;
+            document.getElementById('mySoundButton').innerHTML = "Turn Sound Off"
+        }
+       }
+    }
+})()
+
+document.getElementById('mySoundButton').onclick = function () {
+
+    jakkimodule.switchSound()
+
+}
+
